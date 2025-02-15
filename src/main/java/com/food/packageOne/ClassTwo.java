27797f74e259da7b6c6835f163d796a52c2c44c8
Gopinath7829 <gopinathme042@gmail.com>
2025-02-15 18:45:44 +0530

    commit gopi

---
+++ src/main/java/com/food/packageOne/ClassTwo.java
@@ -0,0 +1,14 @@
+package com.food.packageOne;
+
+public class ClassTwo {
+
+	public ClassTwo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Class Two");
+	}
+
+}
