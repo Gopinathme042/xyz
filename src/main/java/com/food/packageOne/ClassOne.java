55f8c3bb311795f2ddff1bf1281810fa2c876402
Gopinath7829 <gopinathme042@gmail.com>
2025-02-15 18:15:33 +0530

    Commit One

---
+++ src/main/java/com/food/packageOne/ClassOne.java
@@ -0,0 +1,14 @@
+package com.food.packageOne;
+
+public class ClassOne {
+
+	public ClassOne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Print");
+	}
+
+}
---
+++ src/packageOne/ClassOne.java
@@ -0,0 +1,19 @@
+package packageOne;
+
+public class ClassOne {
+
+	public ClassOne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		Thread.sleep(3000);
+		Thread.sleep(3000);
+		Thread.sleep(3000);
+		Thread.sleep(3000);
+		Thread.sleep(3000);
+		System.out.println("Hello World");
+	}
+
+}
